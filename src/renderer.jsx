645a837a80af993b344982d699d9de6adee2bca0
TheScenery <liuwenbin.lwb@gmail.com>
2023-09-18 20:14:18 +0800

    feat: update for default style

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './webview/app';
+import './index.css';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
