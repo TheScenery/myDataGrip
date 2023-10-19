a2aa316c43093ff61d239032d6eb9844ccd9dec2
TheScenery <liuwenbin.lwb@gmail.com>
2023-10-19 21:38:51 +0800

    feat: update for connection properties

--- src/webview/components/ConnectionPropertiesEditor/ConnectionPropertiesEditor.jsx
+++ src/webview/components/ConnectionPropertiesEditor/ConnectionPropertiesEditor.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import { Form, Input } from 'antd';
+import React, { forwardRef, useImperativeHandle } from 'react';
+import { Divider, Form, Input } from 'antd';
 
-export const ConnectionPropertiesEditor = ({ data, onChange }) => {
-  const handleValueChange = (changedValues, allValues) => {
-    onChange(allValues);
+export const ConnectionPropertiesEditor = forwardRef(({ initialValues }, ref) => {
+  const [form] = Form.useForm();
+  const submit = async () => {
+    return await form.validateFields();
   };
+
+  useImperativeHandle(ref, () => ({
+    submit,
+  }));
+
   return (
-      <Form labelCol={{ span: 4 }} onValuesChange={handleValueChange}
-            wrapperCol={{ span: 20 }} labelAlign="left" initialValues={data}>
-        <Form.Item label="Name" name='name'>
+      <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} form={form}
+            labelAlign="left" initialValues={initialValues}>
+        <Form.Item label="Name" name="name" rules={[{ required: true }]}>
           <Input/>
         </Form.Item>
-        <Form.Item label="Comment" name='comment'>
+        <Form.Item label="Comment" name="comment">
+          <Input/>
+        </Form.Item>
+        <Divider/>
+        <Form.Item label="Host" name="host" rules={[{ required: true }]}>
+          <Input/>
+        </Form.Item>
+        <Form.Item label="Port" name="port" rules={[{ required: true }]}>
+          <Input/>
+        </Form.Item>
+        <Form.Item label="User" name="user" rules={[{ required: true }]}>
+          <Input/>
+        </Form.Item>
+        <Form.Item label="Password" name="password" rules={[{ required: true }]}>
+          <Input.Password/>
+        </Form.Item>
+        <Form.Item label="Database" name="database">
           <Input/>
         </Form.Item>
       </Form>
   );
-};
+});
--- src/webview/components/ConnectionPropertiesEditor/EditorDialog.jsx
+++ src/webview/components/ConnectionPropertiesEditor/EditorDialog.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { Modal } from 'antd';
 import { ConnectionPropertiesEditor } from './ConnectionPropertiesEditor';
 
 export const EditorDialog = ({ isOpen, onOk, onClose }) => {
-
-  const [data, setData] = useState({
+  const initialValues = {
     name: 'localhost',
-  });
-  const handleOk = () => {
+    host: 'localhost',
+    port: 3306,
+  };
+
+  const editor = useRef(null);
+  const handleOk = async () => {
+    const data = await editor.current.submit();
     onOk(data);
   };
   return (
       <Modal title="Data Sources and Drivers" open={isOpen} onOk={handleOk}
              onCancel={onClose} destroyOnClose={true} maskClosable={false}>
-        <ConnectionPropertiesEditor data={data} onChange={setData}/>
+        <ConnectionPropertiesEditor initialValues={initialValues} ref={editor}/>
       </Modal>
   );
 };
